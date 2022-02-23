5b188b9ead35bba66342d8b36b8537e213061dbf
Beto333ag <61797533+Beto333ag@users.noreply.github.com>
2022-02-23 03:22:32 -0600

    Add files via upload

---
+++ Assets/texts.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class texts : MonoBehaviour
+{
+    public string animal1;
+    public string bioma;
+    public string emocion;
+    public string animal2;
+    public string AccionFinal;
+    public TextMeshPro TxtRespuesta;
+
+    //habia una vez un "animal1"
+    //que corria muy "emocion"
+    // en un "bioma"  hasta
+    // que se encontro un "animal2"
+    //  estaba "AccionFinal" 
+    // y al no querer molestarlo se fue a su casa
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
